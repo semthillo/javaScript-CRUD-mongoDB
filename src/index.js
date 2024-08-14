68bd19089dae19673f2b7af668726ccfed62b7e2
thillo <salysem90@gmail.com>
2024-08-14 10:28:25 +0200

    last commits

--- src/index.js
+++ src/index.js
@@ -3,62 +3,62 @@ const surveyModule = require("./surveyModule")
 const questionModule = require("./questionModule")
 const answerModule = require("./answerModule")
 
-// const srv = {
-//     surveyId: 6,
-//     name: "Enquête de Satisfaction 002",
-//     description: "Enquête visant à évaluer la satisfaction des clients concernant nos ventes de produits.",
-//     createdAt: new Date(),
-//     createdBy: {
-//         employeeName: "Jane Smith",
-//         employeeRole: "Responsable du service client"
-//     }
-// };
+const srv = {
+    surveyId: 6,
+    name: "Enquête de Satisfaction 002",
+    description: "Enquête visant à évaluer la satisfaction des clients concernant nos ventes de produits.",
+    createdAt: new Date(),
+    createdBy: {
+        employeeName: "Jane Smith",
+        employeeRole: "Responsable du service client"
+    }
+};
 
-// surveyModule.save(srv);
-// surveyModule.listSurvey();
-// surveyModule.destroy(2)
+surveyModule.save(srv);
+surveyModule.listSurvey();
+surveyModule.destroy(2)
 
-// const donne = {
-//     name: "Enquête de Satisfaction ",
-//         description: "Enquête visant à évaluer votre satisfaction .",
-//         createdAt: new Date(),
-//         createdBy: {
-//             employeeName: " Smith",
-//             employeeRole: "Responsable"
-//         }
-//     }
-//     surveyModule.edit(3, donne)
+const donne = {
+    name: "Enquête de Satisfaction ",
+        description: "Enquête visant à évaluer votre satisfaction .",
+        createdAt: new Date(),
+        createdBy: {
+            employeeName: " Smith",
+            employeeRole: "Responsable"
+        }
+    }
+    surveyModule.edit(3, donne)
 
-// const qst = {
-//     questionId: 7,
-//     surveyId: 1,
-//     title: 'Comment évalueriez-vous notre service ?',
-//     type: 'rating',
-//     options: { minValue: 1, maxValue: 5, step: 1 }
-//   }
-//   questionModule.save(qst)
+const qsts = {
+    questionId: 7,
+    surveyId: 1,
+    title: 'Comment évalueriez-vous notre service ?',
+    type: 'rating',
+    options: { minValue: 1, maxValue: 5, step: 1 }
+  }
+  questionModule.save(qsts)
 
-// questionModule.listquestion()
+questionModule.listquestion()
 
-// questionModule.destroy(6)
+questionModule.destroy(6)
 
-// const qst = {
+const qst = {
       
-//     surveyId: 1,
-//     title: 'Comment évalueriez-vous notre service ?',
-//     type: 'rating',
-//     options: { minValue: 1, maxValue: 5, step: 1 }
-//   }
+    surveyId: 1,
+    title: 'Comment évalueriez-vous notre service ?',
+    type: 'rating',
+    options: { minValue: 1, maxValue: 5, step: 1 }
+  }
 
-// questionModule.edit(5, qst)
+questionModule.edit(5, qst)
 
-// const rps =  {
-//     questionId: 8,
-//     title: 'Meilleure reponse'
-//   }
-//   answerModule.save(rps)
+const rpns =  {
+    questionId: 8,
+    title: 'Meilleure reponse'
+  }
+  answerModule.save(rpns)
 
-// answerModule.listanswer()
+answerModule.listanswer()
 
 answerModule.destroy(8)
 
